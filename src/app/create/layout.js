14df54977a683bba3701bbfde7ff62e50c3dca29
wonjangcloud9 <venuscwj@naver.com>
2023-08-08 23:15:09 +0900

    static

--- src/app/create/layout.js
+++ src/app/create/layout.js
@@ -1,8 +1,11 @@
+import Image from "next/image";
+
 const Layout = (props) => {
   return (
     <div>
       <h1>Create</h1>
       {props.children}
+      <Image src="/next.svg" alt="next" width="300" height="300" />
     </div>
   );
 };
